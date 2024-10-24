bab9e713db28aae7bfc5b6d086f301ac09e887a0
albamr09 <bertaaalba1@gmail.com>
2024-10-24 20:28:32 +0200

    [06]: simplify spotlight example

--- src/ch06/04/wall/vs.glsl.ts
+++ src/ch06/04/wall/vs.glsl.ts
@@ -18,13 +18,18 @@ out vec3 vTransformedNormals[numLights];
 void main(void) {
   vec4 vertex = uModelViewMatrix * vec4(aPos, 1.0);
 
+  
+  vec3 normal = vec3(uNormalMatrix * vec4(aNormal, 1.0));
+
   // Iterate over each light
   for(int i = 0; i < numLights; i++) {
     // Define each ray as the vector berween the light and the vertex
     vec4 lightPosition = uModelViewMatrix * vec4(uLightPositions[i], 1.0);
     vRay[i] = vertex.xyz - lightPosition.xyz;
+    // Transform the direction of the light
+    vec3 directionLight = vec3(uNormalMatrix * vec4(uLightDirections[i], 1.0));
     // Transform the normal by substracting the direction of each light
-    vTransformedNormals[i] = vec3(uNormalMatrix * vec4(aNormal - uLightDirections[i], 1.0));
+    vTransformedNormals[i] = normal - directionLight;
   }
 
   gl_Position = uProjectionMatrix * vertex;
