047be0755cbdfa07efcdfa7de58f454cc442cc5e
albamr09 <bertaalba1@gmail.com>
2025-01-13 13:44:52 +0100

    [08] Start second example

---
+++ src/ch08/02/index.ts
@@ -0,0 +1,16 @@
+import { createDescriptionPanel, initGUI } from "../../lib/gui/index";
+
+const initProgram = () => {};
+
+const init = () => {
+  initGUI();
+  createDescriptionPanel(
+    "On this example we will showcase how we can use unique ids in order to determine which object is selected."
+  );
+  // Program
+  // Data
+  // Render
+  // Controls
+};
+
+window.onload = init;
