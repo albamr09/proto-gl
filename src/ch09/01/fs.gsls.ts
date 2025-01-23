68cf8ecdf035738b795114a5af6d934cc5f99f19
albamr09 <bertaalba1@gmail.com>
2025-01-23 11:19:36 +0100

    move color computation to fragment shader

--- src/ch09/01/fs.gsls.ts
+++ src/ch09/01/fs.gsls.ts
@@ -2,12 +2,45 @@ const fragmentShaderSource = `#version 300 es
 
 precision mediump float;
 
-in vec4 vColor;
+const int numLights = 4;
+
+uniform vec4 uMaterialDiffuse;
+uniform vec4 uLightDiffuse;
+uniform vec4 uMaterialAmbient;
+uniform vec4 uLightAmbient;
+uniform vec4 uMaterialSpecular;
+uniform vec4 uLightSpecular;
+uniform float uShininess;
+
+in vec3 vNormal;
+in vec3 vLightRay[numLights];
+in vec3 vEyeVector;
 
 out vec4 fragColor;
 
 void main() {
-  fragColor = vColor;
+
+  vec3 N = normalize(vNormal);
+  vec3 E = normalize(vEyeVector);
+  
+  // Ambient
+  vec4 Ia = uMaterialAmbient * uLightAmbient;
+
+  vec4 Id = vec4(0.0);
+  vec4 Is = vec4(0.0);
+
+  for(int i = 0; i < numLights; i++) {
+    vec3 L = normalize(vLightRay[i]);
+    // Diffuse
+    float lamberTerm = clamp(dot(-L, N), 0.0, 1.0);
+    Id += uMaterialDiffuse * uLightDiffuse * lamberTerm;
+    // Specular
+    vec3 R = reflect(L, N);
+    float specularTerm = max(dot(R, E), 0.0);
+    Is += uMaterialSpecular * uLightSpecular * pow(specularTerm, uShininess);
+  }
+
+  fragColor = vec4((Ia + Id + Is).xyz, 1.0);
 }
 `;
 
--- src/ch09/01/vs.gsls.ts
+++ src/ch09/01/vs.gsls.ts
@@ -10,40 +10,21 @@ in vec3 aPosition;
 in vec3 aNormal;
 
 uniform vec3 uLightPositions[numLights];
-uniform vec4 uMaterialDiffuse;
-uniform vec4 uLightDiffuse;
-uniform vec4 uMaterialAmbient;
-uniform vec4 uLightAmbient;
-uniform vec4 uMaterialSpecular;
-uniform vec4 uLightSpecular;
-uniform float uShininess;
 
 out vec4 vColor;
+out vec3 vNormal;
+out vec3 vEyeVector;
+out vec3 vLightRay[numLights];
 
 void main() {
   vec4 vertex = uModelViewMatrix * vec4(aPosition, 1.0);
   gl_Position = uProjectionMatrix * vertex;
-  vec3 N = normalize(vec3(uNormalMatrix * vec4(aNormal, 1.0)));
-  vec3 eyeVector = -vertex.xyz;
+  vNormal = normalize(vec3(uNormalMatrix * vec4(aNormal, 1.0)));
+  vEyeVector = -vertex.xyz;
 
-  // Ambient
-  vec4 Ia = uMaterialAmbient * uLightAmbient;
-  // Diffuse
-  vec4 Id = vec4(0.0);
   for(int i = 0; i < numLights; i++) {
-    vec3 L = normalize(vertex.xyz - uLightPositions[i]);
-    float lamberTerm = clamp(dot(-L, N), 0.0, 1.0);
-    Id += uMaterialDiffuse * uLightDiffuse * lamberTerm;
+    vLightRay[i] = normalize(vertex.xyz - uLightPositions[i]);
   }
-  // Specular
-  vec4 Is = vec4(0.0);
-  for(int i = 0; i < numLights; i++) {
-    vec3 L = normalize(vertex.xyz - uLightPositions[i]);
-    vec3 R = reflect(L, N);
-    float specularTerm = clamp(dot(R, eyeVector), 0.0, 1.0);
-    Is += uMaterialSpecular * uLightSpecular * pow(specularTerm, uShininess);
-  }
-  vColor = vec4((Ia + Id + Is).xyz, 1.0);
 }
 `;
 
